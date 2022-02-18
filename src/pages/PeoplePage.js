6b389374eb1208938593cd78e049c7e041e77bd9
Ruan carlos <ruan.silva@virtus.ufcg.edu.br>
2022-02-17 21:48:27 -0300

    Added loading and error handling.

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,14 +1,15 @@
-import {View, ActivityIndicator} from 'react-native';
+import {View, ActivityIndicator, StyleSheet, Text} from 'react-native';
 import axios from 'axios';
 import {useEffect, useState} from "react";
 import PeopleList from "../components/PeopleList";
 
 const PeoplePage = (props) => {
 
-    const {navigation} = props;
+    const { navigation } = props;
 
     const [peoples, setPeoples] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         setLoading(true);
@@ -16,20 +17,45 @@ const PeoplePage = (props) => {
             const {results} = response.data;
             setPeoples(results);
             setLoading(false);
+        }).catch(() => {
+            setLoading(false);
+            setError(true);
         });
     }, []);
 
+    const renderPage = () => {
+        if(loading) {
+            return <ActivityIndicator size="large" color="#6ca2f7"/>;
+        }
+
+        if(error) {
+            return <Text style={styles.error}>Something went wrong...</Text>;
+        }
+
+        return <PeopleList
+            peoples={peoples}
+            onPress={(pageParams) => {
+                navigation.navigate('PeopleDetail', pageParams)
+            }}
+        />;
+    };
+
     return (
-        <View>
-            { loading ? <ActivityIndicator size="large" color="#6ca2f7"/> : null }
-            <PeopleList
-                peoples={peoples}
-                onPress={(pageParams) => {
-                    navigation.navigate('PeopleDetail', pageParams)
-                }}
-            />
+        <View style={styles.container}>
+            {renderPage()}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    error: {
+        alignSelf: 'center',
+        fontSize: 20
+    }
+});
 
 export default PeoplePage;
